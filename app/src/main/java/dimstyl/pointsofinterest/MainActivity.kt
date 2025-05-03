9980ba8a8a35e560dfd5b03d6025f2f6664ba875
Dimitris Stylianou <dimitrisstylianou@proton.me>
2025-05-04 00:35:46 +0300

    Update `MainActivity` to use `MainScreen`

--- app/src/main/java/dimstyl/pointsofinterest/MainActivity.kt
+++ app/src/main/java/dimstyl/pointsofinterest/MainActivity.kt
@@ -3,45 +3,21 @@ package dimstyl.pointsofinterest
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
+import androidx.compose.material3.Surface
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
+import dimstyl.pointsofinterest.ui.screens.main.MainScreen
 import dimstyl.pointsofinterest.ui.theme.PointsOfInterestTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         setContent {
             PointsOfInterestTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
+                Surface(Modifier.fillMaxSize()) {
+                    MainScreen(exitApp = { finish() })
                 }
             }
         }
-    }
-}
-
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    PointsOfInterestTheme {
-        Greeting("Android")
     }
 }
