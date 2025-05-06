21aa46bce116a0fe95404a4c4821209d2cd39c4e
Dimitris Stylianou <dimitrisstylianou@proton.me>
2025-05-06 10:52:43 +0300

    Create `PointOfInterestUiModel` data class into `ui\screens\main` package

---
+++ app/src/main/java/dimstyl/pointsofinterest/ui/screens/main/PointOfInterestUiModel.kt
@@ -0,0 +1,14 @@
+package dimstyl.pointsofinterest.ui.screens.main
+
+data class PointOfInterestUiModel(
+    val title: String = "",
+    val category: String = "",
+    val description: String = "",
+    val longitude: Double = 0.0,
+    val latitude: Double = 0.0,
+    val rating: String = "", // min value: 1, max value: 5
+    val minRating: Int = 1,
+    val maxRating: Int = 5,
+    val photo: String = "",
+    val isFavorite: Boolean = false,
+)
