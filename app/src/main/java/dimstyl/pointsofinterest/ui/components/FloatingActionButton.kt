a1c1a1d441789a3fb3afb37a8a8d83407d49d2f0
Dimitris Stylianou <dimitrisstylianou@proton.me>
2025-05-06 10:46:14 +0300

    Update `Color.kt`
    - Rename `IconColor` to `NavBarIconColor`.
    - Add `FloatingActionButtonColor`, `DialogTextFieldFocusedTextColor`, `DialogTextFieldUnfocusedTextColor`, `DialogTextFieldFocusedBorderColor`, `DialogTextFieldUnfocusedBorderColor`, `DialogTextFieldFocusedLabelColor`, `DialogTextFieldUnfocusedLabelColor`, `DialogContainerColor`, `DialogTitleContentColor`, `DialogIconContentColor`, `DialogCameraIconColor`, `DialogFavoriteIconColor`, `DialogConfirmButtonTextColor`, and `DialogDismissButtonTextColor`.

--- app/src/main/java/dimstyl/pointsofinterest/ui/components/FloatingActionButton.kt
+++ app/src/main/java/dimstyl/pointsofinterest/ui/components/FloatingActionButton.kt
@@ -1,12 +1,12 @@
 package dimstyl.pointsofinterest.ui.components
 
-import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.rounded.Add
 import androidx.compose.material3.Icon
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
+import dimstyl.pointsofinterest.R
 import dimstyl.pointsofinterest.ui.theme.AccentColor
-import dimstyl.pointsofinterest.ui.theme.IconColor
+import dimstyl.pointsofinterest.ui.theme.FloatingActionButtonColor
 
 @Composable
 fun FloatingActionButton(modifier: Modifier, onClick: () -> Unit) {
@@ -14,8 +14,11 @@ fun FloatingActionButton(modifier: Modifier, onClick: () -> Unit) {
         modifier = modifier,
         onClick = onClick,
         containerColor = AccentColor,
-        contentColor = IconColor
+        contentColor = FloatingActionButtonColor
     ) {
-        Icon(Icons.Rounded.Add, "Add a new place")
+        Icon(
+            painter = painterResource(R.drawable.add_place),
+            contentDescription = "Add a new place"
+        )
     }
 }
