304dd709245235477d8f4e5a32ea71df5b584e71
Dimitris Stylianou <dimitrisstylianou@proton.me>
2025-05-07 11:36:26 +0300

    Update dependencies
    - Update android gradle plugin (agp) to `8.10.0`.
    - Add `io.coil-kt.coil3:coil-compose:3.1.0` dependency.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -54,6 +54,7 @@ dependencies {
     implementation(libs.androidx.room.runtime)
     implementation(libs.kotlinx.serialization.json)
     implementation(libs.androidx.navigation.compose)
+    implementation(libs.coil.compose)
     ksp(libs.androidx.room.compiler)
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
